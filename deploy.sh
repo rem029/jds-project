492b7c2d283ea69db88c8056f9e92a152787fa5b
Rem Ponce <elawrenceponce@gmail.com>
2023-03-18 11:23:16 +0300

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 echo "Update packages"
 yarn run bootstrap
 
-echo "Update repo"
-git pull origin main
-
 echo "Update database"
 yarn migrate-latest
 
+
 echo "Reloading PM2 Application"
 pm2 reload jds-app-staging
+
+echo "PM2 Update"
+pm2 update
